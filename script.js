c0f36090dd3aadc914ddf14789f4c24081d84284
fabiofgs <fabiofgsdev@gmail.com>
2026-04-08 02:54:27 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function sorteio(){
+    const min = Math.ceil(document.getElementById("min").value);
+    const max = Math.floor(document.getElementById("max").value);
+    const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert("O número sorteado é: " + resultado);
+
+     console.log(min, max);
+}
